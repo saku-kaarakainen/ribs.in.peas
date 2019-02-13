356fe614df1069da52bd20f1a19fdaee2adeef62
saku-kaarakainen <sakuba91@hotmail.com>
2019-02-13 02:32:41 +0200

    Ban the user if the user says an illegal phrase.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-from pprint import pprint
-
+import pprint
+# kik_unofficial API
 from kik_unofficial.client import KikClient
 from kik_unofficial.callbacks import KikClientCallback
 from kik_unofficial.datatypes.xmpp.chatting import IncomingChatMessage, IncomingGroupChatMessage, IncomingStatusResponse, IncomingGroupStatus
@@ -8,32 +8,52 @@ import kik_unofficial.datatypes.xmpp.chatting as chatting
 
 # Fetch the credentials
 with open('../credentials.json') as f:
-    data = json.load(f)
+    credentials = json.load(f)
 
-username = data["username"]
-password = data["password"]
+# Helper methods
+def dump(obj):
+    print(type(obj))
+    print(dir(obj))
+    print(id(obj))
+    print(callable(obj))
+
+def jid_to_username(jid): return jid.split('@')[0][0:-4]
 
 # The main class
 class Rip(KikClientCallback):
     def __init__(self):
-        self.client = KikClient(self, username, password)
+        self.client = KikClient(self, credentials["username"], credentials["password"])
 
     def on_authenticated(self):
         self.client.request_roster()
 
     def on_chat_message_received(self, chat_message: chatting.IncomingChatMessage):
-        if "Rip" in chat_message.body:
-            self.client.send_chat_message(chat_message.from_jid, "Ribs in peas")
-        if "rip" in chat_message.body:
-            self.client.send_chat_message(chat_message.from_jid, "Ribs in peas")
+        print(jid_to_username(chat_message.from_jid) + " (PM): " + chat_message.body) 
+        self.client.send_chat_message(chat_message.from_jid, "You said: " + chat_message.body)
 
     def on_group_message_received(self, chat_message: IncomingGroupChatMessage):
-        print(chat_message.body);
+        ban_text = "Pepe and the fisher man says"
+        if "Pepe and the fisher man says".lower() in  chat_message.body.lower():
+            self.client.remove_peer_from_group(chat_message.group_jid, chat_message.from_jid)
+            self.client.ban_member_from_group(chat_message.group_jid, chat_message.from_jid)
+            self.client.send_chat_message(chat_message.group_jid, "Removed user with JID '" + chat_message.from_jid + "' because the user used an illegal phrase '" + ban_text + "' in the message.")
 
-        if "Rip" in chat_message.body:
-            self.client.send_chat_message(chat_message.group_jid, "Ribs in peas")
-        if "rip" in chat_message.body:
-            self.client.send_chat_message(chat_message.group_jid, "Ribs in peas")
-
-
+# Main
 bot = Rip()
+
+# Note to myself
+"""
+    chat_message: IncomingGroupChatMessage
+        '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+        'body', 
+        'from_jid',
+        'group_jid', 
+        'is_typing', 
+        'message_id', 
+        'metadata', 'preview', 
+        'raw_element', 
+        'request_delivered_receipt', 
+        'request_read_receipt', 
+        'status', 
+        'to_jid'
+"""
